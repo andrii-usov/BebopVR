acb0d65ad4d572a256a84d374fe3fbc77cbcd6a5
valerjanka <adas.ua@gmail.com>
2017-07-12 14:07:15 -0400

    BebopListener

---
+++ app/src/main/java/com/ericsson/bebopvr/dron/BebopListener.java
@@ -0,0 +1,26 @@
+package com.ericsson.bebopvr.dron;
+
+/**
+ * @author valerjanka
+ */
+public interface BebopListener {
+
+    /**
+     * Move drone according new coordinates.
+     * All values are from [-100,100]
+     * Roll: Go Right = 50, Go Left = -50. Rotation around the front-to-back axis
+     * Pitch: Forward = 50, Backward = -10. Rotation around the side-to-side axis
+     * Yaw: Rotate Right = 50, Rotate left = -50. Rotation around the vertical axis (horizontal)
+     * Gaz: Go Up = 50, Go Down = -50.
+     *
+     * @param roll
+     * @param pitch
+     * @param yaw
+     * @param gaz
+     */
+    void move(byte roll, byte pitch, byte yaw, byte gaz);
+
+    void land();
+
+    void takingOff();
+}
