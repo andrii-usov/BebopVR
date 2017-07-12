0307f88d0787c7af0ca287aad4d524e5fb252b22
valerjanka <adas.ua@gmail.com>
2017-07-12 13:04:02 -0400

    Added Main activity. Created BebopDrone and SDCardModule.

---
+++ app/src/main/java/com/ericsson/bebopvr/activity/BebopActivity.java
@@ -0,0 +1,12 @@
+package com.ericsson.bebopvr.activity;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author valerjanka
+ */
+public class BebopActivity extends AppCompatActivity {
+    private static final String TAG = "BebopActivity";
+
+}
